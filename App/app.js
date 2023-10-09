8de9fac1ef2d7f6c637d9d46b35935c8d4855508
Miguel <mig.ruiz@gmail.com>
2023-10-09 18:58:05 +0100

    attic

--- App/app.js
+++ App/app.js
@@ -2,53 +2,31 @@ const { Observable, from, of } = require('rxjs');var mqtt = require('./mqttClust
 const {  map,shareReplay,startWith, filter,switchMap, distinctUntilChanged} = require('rxjs/operators');
 
 global.mtqqLocalPath = process.env.MQTTLOCAL;
+global.mtqqLocalPath = 'mqtt://192.168.0.11';
 
 
 
 
 const rawDoorSensor = new Observable(async subscriber => {  
     var mqttCluster=await mqtt.getClusterAsync()   
-    mqttCluster.subscribeData('zigbee2mqtt/0x00158d0007ecd814', function(content){    
-            subscriber.next(content)
-    });
-});
-
-const masterSwitchSensor = new Observable(async subscriber => {  
-    var mqttCluster=await mqtt.getClusterAsync()   
-    mqttCluster.subscribeData('zigbee2mqtt/0x94deb8fffe57b8ff', function(content){    
+    mqttCluster.subscribeData('zigbee2mqtt/0xa4c13881d0f4ad66', function(content){    
             subscriber.next(content)
     });
 });
 
 
-const doorSensor = rawDoorSensor.pipe( map(m => !m.contact),shareReplay(1))
+
+
+const doorSensor = rawDoorSensor.pipe( map(m => !m.contact))
 
 
 
 
-const masterSwitchStream = masterSwitchSensor.pipe(
-    filter( c=> c.action==='on' || c.action==='brightness_stop' || c.action==='brightness_move_up')
-    ,map(m => m.action==='on')
-    ,startWith(true)
-)
 
-
-const operationStream = masterSwitchStream.pipe(
-    switchMap( ms => {
-        if (ms){
-          return doorSensor
-        }
-        else{
-            return of(ms)
-        }
-           
-    })
-)
-
-operationStream
+doorSensor
 .subscribe(async m => {
     const state = m?"ON":"OFF";
-    (await mqtt.getClusterAsync()).publishData('zigbee2mqtt/0x00124b0024c2eaf7/set',{state})
+    (await mqtt.getClusterAsync()).publishData('zigbee2mqtt/0xa4c138e88b67d876/set',{state})
 })
 
 
